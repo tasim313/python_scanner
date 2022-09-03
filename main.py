2b6b649bcfd77608823cb7344998d8682f4154b2
tasim313 <mostasimmahmudtasim@gmail.com>
2022-09-03 09:05:40 +0600

    source code

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import cv2
 from PIL import Image
 import time
 url = 'http://192.168.2.101:8080/shot.jpg'
-
-
 while True:
     img = request.urlopen(url)
     img_bytes = bytearray(img.read())
     img_np = np.array(img_bytes, dtype=np.uint8)
     frame = cv2.imdecode(img_np, -1)
-    cv2.imshow('Smart Scanner', frame)
-    if cv2.waitKey(1) == ord('s'):
-        print('Save')
-        img_pil = Image.fromarray(frame)
-        time_str = time.strftime('%Y-%m-%d-%H-%M-%S')
-        print(time_str)
-        img_pil.save(f'{time_str}.pdf')
+    frame_cvt = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_blur = cv2.GaussianBlur(frame_cvt, (5, 5), 0)
+    frame_edge = cv2.Canny(frame_blur, 30, 50)
+    contours, _ = cv2.findContours(frame_edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        max_contour = max(contours, key=cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(max_contour)
+        if cv2.contourArea(max_contour) > 50000:
+             # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 3)
+            object_only = frame[y:y+h, x:x+w]
+            cv2.imshow('Smart Scanner', object_only)
+            if cv2.waitKey(1) == ord('s'):
+                 print('Save')
+                 img_pil = Image.fromarray(object_only)
+                 time_str = time.strftime('%Y-%m-%d-%H-%M-%S')
+                 print(time_str)
+                 img_pil.save(f'{time_str}.pdf')
